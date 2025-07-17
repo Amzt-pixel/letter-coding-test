81502374930b3448a58de0b913ce6be218630dee
Amzt-pixel <abishmaraniya@gmail.com>
2025-07-17 23:20:42 +0530

    Update qlltscript.js

--- qlltscript.js
+++ qlltscript.js
@@ -107,7 +107,7 @@ function generateQuestions(num, maxInt) {
         }
 
         if (Math.abs(finalAnswer) <= maxInt) {
-            questions.push({ question: `${letter1} - ${letter2} = ?`, answer: finalAnswer });
+            questions.push({ question: `${letter2} ➞ ${letter1} = ?`, answer: finalAnswer });
         } else {
             i--; // Ensure only valid questions are generated
         }
